57cdb84980b32ef346609a86e13c6642d1c0e00f
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2022-12-10 20:53:39 +0530

    Add files via upload

---
+++ 09-12-2022/Palinodrome.java
@@ -0,0 +1,22 @@
+//Day-4
+import java.util.Scanner;
+class Palinodrome{
+	public static void main(String[] args) {
+		Scanner obj =new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n=obj.nextInt();
+		int r,sum=0,temp;
+		temp=n;
+		while(n>0){
+			r=n%10;
+			sum=(sum*10)+r;
+			n=n/10;
+		}
+		if(temp==sum){
+			System.out.println("is a palinodrome number");
+		}
+		else{
+			System.out.println("is not a palinodrome number");
+		}
+	}
+}
