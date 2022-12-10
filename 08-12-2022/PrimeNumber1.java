b8088a668fe26178b755b69177a2a626571eca00
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2022-12-10 21:56:32 +0530

    Update PrimeNumber1.java

--- 08-12-2022/PrimeNumber1.java
+++ 08-12-2022/PrimeNumber1.java
@@ -1,4 +1,5 @@
 //Day-3
+//Let's see the prime number program in java. In this java program, we will take a number variable and check whether the number is prime or not.
 class PrimeNumber1{
 	static void CheckPrime(int n){
 		int m,i,flag=0;
