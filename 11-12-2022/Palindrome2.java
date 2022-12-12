ceb70bc6b54e0096dfe7dc4d13b07b52cbecda55
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2022-12-12 22:16:09 +0530

    Add files via upload

---
+++ 11-12-2022/Palindrome2.java
@@ -0,0 +1,20 @@
+//Day-6
+//Here,user has to put the string as input to check if the string is palindrome. 
+import java.util.Scanner;
+class Palindrome2{
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.print("Enter a string to check if it is a palindrome:");
+		String str=obj.nextLine();
+		String empty="";
+		for(int i= str.length()-1;i>=0;i--){
+			empty=empty+str.charAt(i);
+		}
+		if(str.equals(empty)){
+			System.out.println("Entered string is a palindrome :"+str);
+		}
+		else{
+			System.out.println("Entered string is not a palindrome:"+str);
+		}
+	}
+}
