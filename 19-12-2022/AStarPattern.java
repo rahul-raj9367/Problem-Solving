c897cc59ad0b53cd4ecf673aeb3dc8eb738ae692
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2022-12-19 22:01:03 +0530

    Update AStarPattern.java

--- 19-12-2022/AStarPattern.java
+++ 19-12-2022/AStarPattern.java
@@ -4,7 +4,6 @@ class AStarPattern{
 	public static void main(String[] args) {
 		int n=5;
 		for(int i=1;i<=n;i++){
-
 			for(int j=1;j<=n;j++){
 				if(i==1||i==(n/2)+1||j==1||j==n){
 					System.out.print("*");
