db23ae7e8f18165a8dea7d14e58413597bc08239
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2022-12-22 20:10:36 +0530

    Add files via upload

---
+++ 22-12-2022/ReverseEachString.java
@@ -0,0 +1,31 @@
+//Day-17
+//Reverse a each word in a string 
+import java.util.Scanner;
+class ReverseEachString{
+	public static void main(String[] args) {
+		Scanner obj =new Scanner(System.in);
+		System.out.print("Enter the String :");
+		String str1=obj.nextLine();
+
+		String sp[] =str1.split(" ");
+		//[Good,Evening]
+		String revstring="";
+	
+		for(int i=0;i<sp.length;i++){
+			String word=sp[i]; 
+			String revword="";
+
+			for(int j=word.length()-1;j>=0;j--){
+				revword=revword+word.charAt(j);
+			}
+			revstring=revstring+revword+" ";
+		}
+		System.out.println(revstring);
+	}
+}
+/*
+INPUT-Good Evening
+
+OUTPUT-dooG gninevE
+
+*/
