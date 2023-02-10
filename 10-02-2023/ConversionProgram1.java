07e26b0f14cc957d7b0987495761de667de7967e
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-10 22:01:03 +0530

    Add files via upload

---
+++ 10-02-2023/ConversionProgram1.java
@@ -0,0 +1,16 @@
+//Day-66
+//Conversion Programs
+//Java Convert String to int
+class ConversionProgram1{
+	public static void main(String[] args) {
+		String s="200";  
+		//Converting String into int using Integer.parseInt()  
+		int i=Integer.parseInt(s);  
+		//Printing value of i  
+		System.out.println(i); 
+	}
+}
+/*
+OUTPUT
+200
+*/
