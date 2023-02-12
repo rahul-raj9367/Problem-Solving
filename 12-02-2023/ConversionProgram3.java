684485cd0a2b3a03a90c3eb2156abb3fc382fdcf
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-12 22:40:31 +0530

    Add files via upload

---
+++ 12-02-2023/ConversionProgram3.java
@@ -0,0 +1,14 @@
+//Day-68
+//Conversion Programs
+//Java String to long
+class ConversionProgram3{
+	public static void main(String[] args) {
+		String s="9990449935";  
+		long l=Long.parseLong(s);  
+		System.out.println(l); 
+	}
+}
+/*
+OUTPUT
+9990449935
+*/
