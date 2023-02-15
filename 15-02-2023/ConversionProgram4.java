63f7f8cc9b8fa5f6362a86f54888e2e57852a4cd
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-15 22:13:22 +0530

    Add files via upload

---
+++ 15-02-2023/ConversionProgram4.java
@@ -0,0 +1,14 @@
+//Day-72
+//String Program
+//Java long to String
+class ConversionProgram4{
+	public static void main(String[] args) {
+		long l=12345678;
+		String s=String.valueOf(l);
+		System.out.println(s);
+	}
+}
+/*
+OUTPUT
+12345678
+*/
---
+++ 15-02-2023/ConversionProgram5.java
@@ -0,0 +1,14 @@
+//Day-72
+//String Program
+//Java String to float Example
+class ConversionProgram5{
+	public static void main(String[] args) {
+		String s ="12345";
+		float f=Float.parseFloat(s);
+		System.out.println(f);
+	}
+}
+/*
+OUTPUT
+12345.0
+*/
