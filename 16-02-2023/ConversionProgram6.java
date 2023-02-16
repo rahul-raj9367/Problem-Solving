26f6edc717eae333a1620a88cac8d6144a1e1ae8
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-16 22:15:16 +0530

    Add files via upload

---
+++ 16-02-2023/ConversionProgram6.java
@@ -0,0 +1,14 @@
+//Day-73
+//String Program
+//Java Convert String to double
+class ConversionProgram6{
+	public static void main(String[] args) {
+		String s="12345";
+		Double d=Double.parseDouble(s);
+		System.out.println(d); 
+	}
+}
+/*
+OUTPUT
+12345.0
+*/
---
+++ 16-02-2023/ConversionProgram7.java
@@ -0,0 +1,14 @@
+//Day-73
+//String Program
+//Java Convert double to String
+class ConversionProgram7{
+	public static void main(String[] args) {
+		double d= 9.22345623;
+		String s=String.valueOf(d);
+		System.out.println(s);
+	}
+}
+/*
+OUTPUT
+9.22345623
+*/
