8bf63b32e94c83250ca5f0f790e18fcf68b493e0
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-20 22:24:02 +0530

    Add files via upload

---
+++ 20-02-2023/ConversionProgram8.java
@@ -0,0 +1,14 @@
+//Day 77
+//Conversion Program
+//Java Convert int to long
+class ConversionProgram8{
+	public static void main(String[] args) {
+		int i=200;  
+		long l=i;  
+		System.out.println(l); 
+	}
+}
+/*
+OUTPUT
+200
+*/
