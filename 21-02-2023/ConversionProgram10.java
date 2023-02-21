86c1997528c7ae4089ed209fa1a4db1b9251a319
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-21 20:06:52 +0530

    Add files via upload

---
+++ 21-02-2023/ConversionProgram10.java
@@ -0,0 +1,14 @@
+//Day 78
+//Conversion Program
+//Java Convert String to char
+class ConversionProgram10{
+	public static void main(String[] args) {
+		String s="Today Heavy Fever";
+		char ch=s.charAt(6);
+		System.out.println(ch);
+	}
+}
+/*
+OUTPUT
+H
+*/
---
+++ 21-02-2023/ConversionProgram9.java
@@ -0,0 +1,14 @@
+//Day 78
+//Conversion Program
+//Java Convert long into int
+class ConversionProgram9{
+	public static void main(String[] args) {
+		long l=500;
+		int i=(int)l;
+		System.out.println(l);
+	}
+}
+/*
+OUTPUT
+500
+*/
