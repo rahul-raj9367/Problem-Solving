2fe76e71ee80fb752b74d0823ad18cfd6c7ce6c2
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-26 22:18:33 +0530

    Add files via upload

---
+++ 26-02-2023/SinglyLinkedList.java
@@ -0,0 +1,14 @@
+//Day 83
+//Implement A Singly Linked List 
+public class SinglyLinkedList{
+	private ListNode head;
+	private static class ListNode{
+		private int data;//Generic
+		private ListNode next;
+
+		public ListNode(int data){
+			this.data=data;
+			this.next=null;
+		}
+	}
+}
