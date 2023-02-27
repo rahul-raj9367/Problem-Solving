1189ee39e7bfec5938f33226a821f9c7749a781b
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-27 23:13:25 +0530

    Add files via upload

---
+++ 27-02-2023/ConversionProgram11.java
@@ -0,0 +1,18 @@
+//Day 83
+//Conversion Program
+//Java Convert char to int Java char to int Example: Get ASCII value
+class ConversionProgram11{
+	public static void main(String[] args) {
+		char c='a';  
+		char c2='1';  
+		int a=c;  
+		int b=c2;  
+		System.out.println(a);  
+		System.out.println(b);  
+	}
+}
+/*
+OUTPUT
+97
+49
+*/
