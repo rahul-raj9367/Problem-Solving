95e98443e22de639f7108de650b11833ba4e7840
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-02-28 22:12:02 +0530

    Add files via upload

---
+++ 28-02-2023/ConversionProgram12.java
@@ -0,0 +1,14 @@
+//Day 85
+//Conversion Program
+//Java Convert char to int
+class ConversionProgram12{
+	public static void main(String[] args) {
+		char c='1';  
+		int a=Character.getNumericValue(c);  
+		System.out.println(a);
+	}
+}
+/*
+OUTPUT
+1
+*/
