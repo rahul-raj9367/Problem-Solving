14d1777d65dc6d1dabc6da80d34283bd6485b885
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-03-02 22:35:45 +0530

    Add files via upload

---
+++ 02-03-2023/ConversionProgram14.java
@@ -0,0 +1,20 @@
+//Day 87
+//Conversion Program
+//Print ASCII Value in Java
+class ConversionProgram14{
+	public static void main(String[] args) {
+		// character whose ASCII value to be found  
+		char ch1 = 'a';  
+		char ch2 = 'b';  
+		// variable that stores the integer value of the character  
+		int asciivalue1 = ch1;  
+		int asciivalue2 = ch2;  
+		System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+		System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+	}
+}
+/*
+OUTPUT
+The ASCII value of a is: 97
+The ASCII value of b is: 98
+*/
