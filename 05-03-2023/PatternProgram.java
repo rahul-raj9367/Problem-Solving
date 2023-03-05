847e7217ec1f1752bddd36d5195dec845345c37d
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-03-05 21:54:50 +0530

    Add files via upload

---
+++ 05-03-2023/PatternProgram.java
@@ -0,0 +1,30 @@
+//Day 89
+//Java program to print the following pattern on the console
+class PatternProgram{
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++){
+			for(int j=0;j<10;j++){
+				if(i==0 || i==9 || j==0 || j==9){
+					System.out.print("1");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+OUTPUT
+1111111111
+1        1
+1        1
+1        1
+1        1
+1        1
+1        1
+1        1
+1        1
+1111111111
+*/
