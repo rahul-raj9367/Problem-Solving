66258d452f4864105e508075c04400870772e519
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-03-08 22:21:04 +0530

    Add files via upload

---
+++ 08-03-2023/PatternProgram1.java
@@ -0,0 +1,23 @@
+//Day 90
+//StartPattern Program
+class PatternProgram1{
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++){
+			for(int k=5;k>i;k--){
+				System.out.print(" ");
+			}
+			for(int j=0;j<=i;j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+OUTPUT
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+*/
