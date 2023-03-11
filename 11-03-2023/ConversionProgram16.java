6e8db13f00614cc313dda2d3115994beba11017f
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-03-11 22:25:26 +0530

    Add files via upload

---
+++ 11-03-2023/ConversionProgram16.java
@@ -0,0 +1,16 @@
+//Day 93
+//Conversion Program
+//Java Convert Decimal to Hexadecimal
+class ConversionProgram16{
+	public static void main(String[] args) {
+		System.out.println(Integer.toHexString(10));  
+		System.out.println(Integer.toHexString(15));  
+		System.out.println(Integer.toHexString(289));  
+	}
+}
+/*
+OUTPUT
+a
+f
+121
+*/
