e0197b7b21c15e1b07e52ae7a96b44fe9ebf21bd
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-03-13 22:14:32 +0530

    Add files via upload

---
+++ 13-03-2023/ConversionProgram18.java
@@ -0,0 +1,14 @@
+//Day 95
+//Conversion Program
+//Java Convert Octal to Decimal
+class ConversionProgram18{
+	public static void main(String[] args) {
+		String octalString="121"; 
+		int decimal=Integer.parseInt(octalString,8);   
+		System.out.println(decimal); 
+	}
+}
+/*
+OUTPUT
+81
+*/
