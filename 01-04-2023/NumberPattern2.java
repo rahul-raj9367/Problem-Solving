71ed7ee37d7c9ab3ac912bdb4bc7e9aea405f55f
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-04-01 21:12:56 +0530

    Add files via upload

---
+++ 01-04-2023/NumberPattern2.java
@@ -0,0 +1,25 @@
+//Day-109
+//NumberPattern
+class NumberPattern2{
+	public static void main(String[] args) {
+		int k=1;
+		for(int i=1;i<=7;i++){
+			//Prints Numbers
+			for(int j=1;j<=i;j++){
+				System.out.print(k +" ");
+				k++;
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+OUTPUT
+1
+2 3
+4 5 6 
+7 8 9 10
+11 12 13 14 15
+16 17 18 19 20 21
+22 23 24 25 26 27 28
+*/
